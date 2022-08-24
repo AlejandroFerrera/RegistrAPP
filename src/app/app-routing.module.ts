a4934a8aa4df0de5854fb5dda5fcccd797c65f92
kcaycho <kcaycho@gmail.com>
2022-08-24 00:06:36 -0400

    add alumnos pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'alumnos',
+    loadChildren: () => import('./alumnos/alumnos.module').then( m => m.AlumnosPageModule)
+  },
 
 ];
 
