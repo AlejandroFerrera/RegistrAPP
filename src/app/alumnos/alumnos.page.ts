70f3f7f1242b8a1d404f9ea6429a81806a42a06f
kcaycho <kcaycho@gmail.com>
2022-08-27 23:24:37 -0400

    animation click

--- src/app/alumnos/alumnos.page.ts
+++ src/app/alumnos/alumnos.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController,NavController,AnimationController,createAnimation} from '@ionic/angular';
+
 
 @Component({
   selector: 'app-alumnos',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlumnosPage implements OnInit {
 
-  constructor() { }
+  constructor(private animationCtrl: AnimationController) { }
 
   ngOnInit() {
+
+  /*animacion*/
+  
+  const animationClick = this.animationCtrl.create()
+  .addElement(document.getElementById("animationClick"))
+  .fill('none')
+  .duration(1500)
+  .iterations(Infinity)
+  .keyframes([
+    { offset: 0, transform: 'scale(1)', opacity: '1' },
+    { offset: 0.5, transform: 'scale(1.1)', opacity: '0.9' },
+    { offset: 1, transform: 'scale(1)', opacity: '1' }
+  ]);
+
+  animationClick.play()
+
   }
 
 }
